762429a64759abd290f481a733e9d091a22bf0fa
dasha <d.ignatushko11@gmail.com>
2025-03-22 13:26:19 +0200

    added methods for button

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ class LikeApp(App):
         btnBox=BoxLayout()
         lbl_title=Label(text="Як тобі цей кіт??????", font_size=32, halign="center", size_hint=[1,0.1])
 
-        btn_like=Button(text="Like", font_size=24, size_hint=[0.5,0.3])
-        btn_dislike=Button(text="Dislike", font_size=24, size_hint=[0.5,0.3])
+        btn_like=Button(text="Like", font_size=24, size_hint=[0.5,0.5], on_press=self.like)
+        btn_dislike=Button(text="Dislike", font_size=24, size_hint=[0.5,0.5], on_press=self.dislike)
 
         self.img=Image(source="photo/cat2.jpg", size_hint=[1,0.6])
         self.mainBox.add_widget(lbl_title)
@@ -23,6 +23,10 @@ class LikeApp(App):
 
         self.mainBox.add_widget(btnBox)
         return self.mainBox
+    def like(self,btn):
+        print("liked")
+    def dislike(self,btn):
+        print("disliked")
 
 LikeApp().run()
 

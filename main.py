339d6e2934b919edd40ee8fb9f1db252317b3d92
gooseDes <166229741+gooseDes@users.noreply.github.com>
2025-03-22 13:26:21 +0200

    added methods for buttons

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ class FindYourCatApp(App):
         btnBox=BoxLayout()
         lbl_title = Label(text="Оцініть картинку", font_size=32, halign="center", size_hint=[1,0.1])
 
-        btn_like = Button(text="like", font_size = 24, size_hint=[0.5, 0.3])
-        btn_dislike = Button(text='dislike', font_size=24, size_hint=[0.5,0.3])
+        btn_like = Button(text="like", font_size = 24, size_hint=[0.5, 0.3], on_press=self.like)
+        btn_dislike = Button(text='dislike', font_size=24, size_hint=[0.5,0.3], on_press=self.dislike)
 
         self.img = Image(source="89534151-13837875-image-a-22_1726052440056.png", size_hint=[1, 0.6])
 
@@ -25,4 +25,8 @@ class FindYourCatApp(App):
         self.mainBox.add_widget(btnBox)
         return self.mainBox
     
+    def like(self, btn):
+        print("like")
+    def dislike(self, btn):
+        print("dislike")
 FindYourCatApp().run()

5e5445c956b4460c7e8e7ceacb0f8e8a2d602917
gooseDes <166229741+gooseDes@users.noreply.github.com>
2025-03-29 13:18:47 +0200

    stars add

--- main.py
+++ main.py
@@ -5,28 +5,39 @@ from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 
-class FindYourCatApp(App):
-    def build(self):
-        self.mainBox = BoxLayout(orientation='vertical')
+class Rate(BoxLayout):
+    def __init__(self):
+        super().__init__()
+        self.orientation="vertical"
         btnBox=BoxLayout()
         lbl_title = Label(text="Оцініть картинку", font_size=32, halign="center", size_hint=[1,0.1])
 
-        btn_like = Button(text="like", font_size = 24, size_hint=[0.5, 0.3], on_press=self.like)
-        btn_dislike = Button(text='dislike', font_size=24, size_hint=[0.5,0.3], on_press=self.dislike)
+        self.img = Image(source="photo\pexels-kmerriman-20787.jpg", size_hint=[1, 0.6])
 
-        self.img = Image(source="89534151-13837875-image-a-22_1726052440056.png", size_hint=[1, 0.6])
+        self.add_widget(lbl_title)
+        self.add_widget(self.img)
+        self.buttons=[]
+        for i in range(5):
+            btn=Button(text=str(i+1), background_normal="images\star0.png",color = [0,0,0,0], background_down="images\star0.png")
+            self.buttons.append(btn)
+            btnBox.add_widget(btn)
+            btn.bind(on_press=self.rating)
+        self.add_widget(btnBox)
 
-        self.mainBox.add_widget(lbl_title)
-        self.mainBox.add_widget(self.img)
-
-        btnBox.add_widget(btn_like)
-        btnBox.add_widget(btn_dislike)
-
-        self.mainBox.add_widget(btnBox)
+    def rating(self, btn):
+        index = int(btn.text)-1
+        for i in range(len(self.buttons)):
+            if i<= index:
+                self.buttons[i].background_normal="images\star1.png"
+                self.buttons[i].background_down="images\star1.png"
+            else:
+                self.buttons[i].background_normal="images\star0.png"
+                self.buttons[i].background_down="images\star0.png"
+class FindYourCatApp(App):
+    def build(self):
+        self.mainBox = BoxLayout(orientation='vertical')
+        rate=Rate()
+        self.mainBox.add_widget(rate)
         return self.mainBox
-    
-    def like(self, btn):
-        print("like")
-    def dislike(self, btn):
-        print("dislike")
+
 FindYourCatApp().run()
